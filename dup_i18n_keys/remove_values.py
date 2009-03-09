3351d163dbcd89761f8252cbe1ce6645e64f18e1
paulproteus <paulproteus@committer.creativecommons.org>
2009-03-09 20:17:13 +0000

    one-off written; will run it momentarily

--- dup_i18n_keys/remove_values.py
+++ dup_i18n_keys/remove_values.py
@@ -1,26 +1,25 @@
 import babel.messages.pofile
 import collections
-import dup_i18n_keys
 
 def main():
     import sys
-    filename, duplicates = sys.argv[1], sys.argv[2:]
-    print 'Going to remove', duplicates, 'from', filename
-    remove_dup_keys(filename, duplicates)
+    filename = sys.argv[1]
+    print 'Going to remove values from', filename
+    remove_values(filename)
     print 'YOW! Finished.'
 
-def remove_dup_keys(filename, remove_this):
+def remove_values(filename):
     # Verify that each of the remove_this things all the duplicates
-    remove_this = set(remove_this)
 
     # Actually remove
     pofile = babel.messages.pofile.read_po(open(filename))
-    for key in remove_this:
-        del pofile._messages[key]
+    for key in pofile._messages:
+        pofile._messages[key].string = ''
 
     # Now recklessly clobber that input filename
     out_fd = open(filename, 'w')
     babel.messages.pofile.write_po(out_fd, pofile)
+    out_fd.close()
 
 if __name__ == '__main__':
     main()
